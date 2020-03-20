79fe3b89071b8494d098628b409aa70bd5a15edc
viz <visuweshmuthukumar@gmail.com>
2020-03-20 23:13:03 +0530

    minor changes

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* modifier 0 means no modifier */
 static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
 static char *fulluseragent  = ""; /* Or override the whole user agent string */
-static char *scriptfile     = "~/.surf/script.js";
-static char *styledir       = "~/.surf/styles/";
-static char *certdir        = "~/.surf/certificates/";
-static char *cachedir       = "~/.surf/cache/";
-static char *cookiefile     = "~/.surf/cookies.txt";
+static char *scriptfile     = "~/lib/surf/script.js";
+static char *styledir       = "~/lib/surf/styles/";
+static char *certdir        = "~/lib/surf/certificates/";
+static char *cachedir       = "~/lib/surf/cache/";
+static char *cookiefile     = "~/lib/surf/cookies.txt";
 
 /* Webkit default features */
 /* Highest priority value will be used.
